41caad700d64573de8f581da933791a176f45b5f
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-05-01 15:24:53 +0300

    updates

--- AppStore/Controller/AppsSearchController.swift
+++ AppStore/Controller/AppsSearchController.swift
@@ -45,8 +45,7 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! SearchResultCell
-        let appResult = appResults[indexPath.item]
-        cell.appResult = appResult
+        cell.appResult = appResults[indexPath.item]
         return cell
     }
     
