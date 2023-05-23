3dd88629b59bb83a023defe3981d1277f89de37d
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-05-23 21:19:26 +0300

    Fetchind data and populate the cell with data

---
+++ AppStore/Controller/Today/TodayMultipleAppController.swift
@@ -0,0 +1,8 @@
+//
+//  TodayMultipleAppController.swift
+//  AppStore
+//
+//  Created by Murat Ceyhun Korpeoglu on 23.05.2023.
+//
+
+import Foundation
---
+++ AppStore/Views/Today/MultipleAppCell.swift
@@ -0,0 +1,8 @@
+//
+//  MultipleAppCell.swift
+//  AppStore
+//
+//  Created by Murat Ceyhun Korpeoglu on 23.05.2023.
+//
+
+import Foundation
